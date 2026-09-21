a606ca0a9c849665ef72a2416cbb97388d2920d3
zantetsukenodin12-art <zantetsukenodin12@gmail.com>
2026-09-21 21:59:43 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "ambatukammm",     // this is what people actually see
+        state: "Walang kanin busenggg",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
